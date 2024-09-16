29828c3f33a3942cd7e8c8ce8519c4392f14a8c0
jagadees-reddy <jagadeeswar.reddy3@wipro.com>
2024-09-16 21:08:43 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,33 +1,23 @@
 #!/usr/bin/env python
 
-# -*- coding: utf-8 -*-
-
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
 from setuptools import setup, find_packages
 
 readme = open('README.md').read()
 
 setup(
-    # Change these when cloning this repo!
-    name='generic-docker-python',
+    name='python_application',
     description='A sample application',
     author='Frank Bertsch',
     author_email='frank@mozilla.com',
-    url='https://github.com/fbertsch/generic-docker-python',
+    url='https://github.com/jagadees-reddy/generic-python-docker',
     packages=find_packages(include=['python_application']),
-    package_dir={'python-application': 'python_application'},
     entry_points={
         'console_scripts': [
-            'python-application=python_application.__main__:main',
+            'python_application=python_application.__main__:main',
         ],
     },
-
-    # These don't necessarily need changed
     python_requires='>=3.6.0',
-    version='0.0.0',
+    version='0.0.1',
     long_description=readme,
     include_package_data=True,
     install_requires=[
