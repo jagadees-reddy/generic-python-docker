9bf43c7c51422ce4dc3529bdd07a780b43e429d0
jagadees-reddy <jagadeeswar.reddy3@wipro.com>
2024-09-16 15:44:03 +0100

    Update test_application.py

--- tests/test_application.py
+++ tests/test_application.py
@@ -1,6 +1,6 @@
+# generic-python-docker/tests/test_application.py
 import pytest
 
-#from python_application.application import App
 from generic_python_docker import application  # Ensure this import is correct
 
 
@@ -11,5 +11,5 @@ def app():
 
 class TestApplication(object):
 
-    def test_return_value(self, application):
-        assert application.get_hello_world() == "Hello, World"
+def test_return_value(self, application):
+    assert application.get_hello_world() == "Hello, World"
