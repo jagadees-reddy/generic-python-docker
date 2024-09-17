e8511385c92d7821743090da1c9f47f185ffdda5
jagadees-reddy <jagadeeswar.reddy3@wipro.com>
2024-09-17 16:50:37 +0100

    Update test_app.py

--- harness/tests/test_app.py
+++ harness/tests/test_app.py
@@ -1,1 +1,12 @@
+# tests/test_app.py
 
+import pytest
+from python_application.application import App
+
+@pytest.fixture
+def app():
+    return App()
+
+class TestApplication:
+    def test_return_value(self, app):
+        assert app.get_hello_world() == "Hello, World"
